a106730ebb499f7fba0d79d929db9c77ebde2f18
Vitor de Paula <vitordepaula98@hotmail.com>
2025-04-14 12:14:41 -0300

    fix: corrigido erro de tipagem

--- src/utils/supabase/client.ts
+++ src/utils/supabase/client.ts
@@ -1,8 +1,15 @@
 import { createBrowserClient } from "@supabase/ssr";
 
 export function createClient() {
+	if (
+		!process.env.NEXT_PUBLIC_SUPABASE_URL ||
+		!process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+	) {
+		throw new Error("Faltam variáveis de ambiente necessárias para o Supabase");
+	}
+
 	return createBrowserClient(
-		process.env.NEXT_PUBLIC_SUPABASE_URL!,
-		process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+		process.env.NEXT_PUBLIC_SUPABASE_URL,
+		process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
 	);
 }
--- src/utils/supabase/server.ts
+++ src/utils/supabase/server.ts
@@ -3,10 +3,9 @@ import { cookies } from "next/headers";
 
 export async function createClient() {
 	const cookieStore = await cookies();
-
 	return createServerClient(
-		process.env.NEXT_PUBLIC_SUPABASE_URL!,
-		process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+		process.env.NEXT_PUBLIC_SUPABASE_URL ?? "",
+		process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ?? "",
 		{
 			cookies: {
 				getAll() {
@@ -14,9 +13,9 @@ export async function createClient() {
 				},
 				setAll(cookiesToSet) {
 					try {
-						cookiesToSet.forEach(({ name, value, options }) =>
-							cookieStore.set(name, value, options),
-						);
+						for (const { name, value, options } of cookiesToSet) {
+							cookieStore.set(name, value, options);
+						}
 					} catch {
 						// The `setAll` method was called from a Server Component.
 						// This can be ignored if you have middleware refreshing
