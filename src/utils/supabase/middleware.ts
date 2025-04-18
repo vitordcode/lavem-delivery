3615cff84418e5b7cd6cdfa38186bbd0e8d711f1
Vitor de Paula <vitordepaula98@hotmail.com>
2025-04-18 17:01:15 -0300

    feat: configurado algumas rotas para auth/supabase

--- src/utils/supabase/middleware.ts
+++ src/utils/supabase/middleware.ts
@@ -7,23 +7,27 @@ export async function updateSession(request: NextRequest) {
 	});
 
 	const supabase = createServerClient(
-		process.env.NEXT_PUBLIC_SUPABASE_URL ?? "",
-		process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ?? "",
+		// biome-ignore lint/style/noNonNullAssertion: <explanation>
+		process.env.NEXT_PUBLIC_SUPABASE_URL!,
+		// biome-ignore lint/style/noNonNullAssertion: <explanation>
+		process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
 		{
 			cookies: {
 				getAll() {
 					return request.cookies.getAll();
 				},
 				setAll(cookiesToSet) {
-					for (const { name, value } of cookiesToSet) {
-						request.cookies.set(name, value);
-					}
+					// biome-ignore lint/complexity/noForEach: <explanation>
+					cookiesToSet.forEach(({ name, value, options }) =>
+						request.cookies.set(name, value),
+					);
 					supabaseResponse = NextResponse.next({
 						request,
 					});
-					for (const { name, value, options } of cookiesToSet) {
-						supabaseResponse.cookies.set(name, value, options);
-					}
+					// biome-ignore lint/complexity/noForEach: <explanation>
+					cookiesToSet.forEach(({ name, value, options }) =>
+						supabaseResponse.cookies.set(name, value, options),
+					);
 				},
 			},
 		},
